e6febeb487be812cafec39ff13dc4bf2ec035a53
Ronald Aug <saisenfa@gmail.com>
2019-08-15 15:41:32 +0630

    remove letter-spacin 👨🏿‍💻

--- js/min.js
+++ js/min.js
@@ -4,15 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const allP = document.querySelectorAll('p');
     const allH = document.querySelectorAll('h1,h2,h3,h4,h5');
     const allG = document.querySelectorAll('span,li,td');
-   function changeFonts(){
-        loopContentArr(allA);
-        loopContentArr(allP);
-        loopContentArr(allH);
-        loopContentArr(allG);
-    }
   
-   // loop and change all content
-   function loopContentArr(content){
+    // loop and change all content
+    function loopContentArr(content){
       if(!content){return;}
       const currentFont = localStorage.getItem('current-font') || 'unicode';
       content.forEach(e=>{
@@ -27,28 +21,46 @@ document.addEventListener("DOMContentLoaded", function() {
               }
           }
       })
-   }
-  
-   // Listen user selected font on Change
-   const selectBox = document.querySelector("#auto-font");
-   if(!selectBox){return}
-   selectBox.addEventListener('change',function(e){
-       if(e.target.value !== 'uni'){
-            localStorage.setItem('current-font','zawgyi');
-            changeFonts();
-       }else{
-            localStorage.setItem('current-font','unicode');
-            changeFonts();
-       }
-       isOne = true;
-   });
-   if(!isOne){
-     changeFonts();
-   }
-   const currentFont = localStorage.getItem('current-font') || 'unicode';
-        if(currentFont !== 'unicode'){
-            selectBox.value = 'zaw';
+    }
+
+    function changeFonts(){
+        loopContentArr(allA);
+        loopContentArr(allP);
+        loopContentArr(allH);
+        loopContentArr(allG);
+    }
+
+    function removeLS(){
+        const ls = document.createElement("style");
+        ls.id = "auto-font-style";
+        ls.innerHTML = `
+        body,p,a,h1,h2,h3,h4,h5{
+            letter-spacing:normal;
+        }`;
+        document.head.append(ls);
+    }
+    removeLS();
+
+    // Listen user selected font on Change
+    const selectBox = document.querySelector("#auto-font");
+    if(!selectBox){return}
+    selectBox.addEventListener('change',function(e){
+        if(e.target.value !== 'uni'){
+                localStorage.setItem('current-font','zawgyi');
+                changeFonts();
         }else{
-            selectBox.value = 'uni';
+                localStorage.setItem('current-font','unicode');
+                changeFonts();
         }
+        isOne = true;
+    });
+    if(!isOne){
+        changeFonts();
+    }
+    const currentFont = localStorage.getItem('current-font') || 'unicode';
+    if(currentFont !== 'unicode'){
+        selectBox.value = 'zaw';
+    }else{
+        selectBox.value = 'uni';
+    }
   });
